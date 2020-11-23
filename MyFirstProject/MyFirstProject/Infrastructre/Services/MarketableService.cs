12e965327e66023e221aee62af0a036e624282b8
Elmar Abdullayev <elmarsha@code.edu.az>
2020-11-23 09:27:12 +0400

    Created MarketableService

---
+++ MyFirstProject/MyFirstProject/Infrastructre/Services/MarketableService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MyFirstProject.Infrastructre.Interfaces;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.Infrastructre.Services
+{
+   public class MarketableService:IMarketable
+    {
+    }
+}
